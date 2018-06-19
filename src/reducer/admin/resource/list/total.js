3fa7648971ac525757e15586c029728d7d00ace1
Jake Dluhy <dluhy.jake@gmail.com>
2018-06-19 15:29:18 -0700

    Fix total and totalAll counts for other actions (#34)

--- src/reducer/admin/resource/list/total.js
+++ src/reducer/admin/resource/list/total.js
@@ -1,4 +1,7 @@
 import {
+    CRUD_CREATE_SUCCESS,
+    CRUD_DELETE_SUCCESS,
+    CRUD_GET_MANY_REFERENCE_SUCCESS,
     CRUD_GET_ONE_SUCCESS,
     CRUD_GET_LIST_SUCCESS,
     CRUD_DELETE_OPTIMISTIC,
@@ -9,17 +12,23 @@ export default resource => (previousState = 0, { type, payload, meta }) => {
     if (!meta || meta.resource !== resource) {
         return previousState;
     }
-    if (type === CRUD_GET_ONE_SUCCESS) {
-        return previousState == 0 ? 1 : previousState;
+
+    switch(type) {
+        case CRUD_GET_ONE_SUCCESS:
+            return previousState == 0 ? 1 : previousState;
+        case CRUD_GET_LIST_SUCCESS:
+            return payload.total || previousState;
+        case CRUD_GET_MANY_REFERENCE_SUCCESS:
+            return payload.total || previousState;
+        case CRUD_CREATE_SUCCESS:
+            return previousState + 1;
+        case CRUD_DELETE_SUCCESS:
+            return previousState - 1;
+        case CRUD_DELETE_OPTIMISTIC:
+            return previousState - 1;
+        case CRUD_DELETE_MANY_OPTIMISTIC:
+            return previousState - payload.ids.length;
+        default:
+            return previousState;
     }
-    if (type === CRUD_GET_LIST_SUCCESS) {
-        return payload.total;
-    }
-    if (type === CRUD_DELETE_OPTIMISTIC) {
-        return previousState - 1;
-    }
-    if (type === CRUD_DELETE_MANY_OPTIMISTIC) {
-        return previousState - payload.ids.length;
-    }
-    return previousState;
 };
